a720af1473220a52d10c5637eaba74f52fc846fc
Abdelrahman-bedier <abdo.bedier@gmail.com>
2024-08-18 22:41:48 +0300

    name

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class Author(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=64)
-
+    def __str__(self):
+        return self.name 
+    
 class Book(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=64)
