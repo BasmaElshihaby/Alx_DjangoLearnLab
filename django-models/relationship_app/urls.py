15ce142a7ef3dc592285718d8adc3f9c8e8eec19
Abdelrahman-bedier <abdo.bedier@gmail.com>
2024-08-25 23:40:41 +0300

    url

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
-from .admin_view import admin_view
-from .librarian_view import librarian_view
-from .member_view import member_view
 
 urlpatterns = [
     path('books/', views.list_books, name='books'),
@@ -11,7 +8,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
-    path('admin/', admin_view, name='admin'),
-    path('librarian/', librarian_view, name='librarian'),
-    path('member/', member_view, name='member'),
+    path('admin/', views.admin_view, name='admin'),
+    path('librarian/', views.librarian_view, name='librarian'),
+    path('member/', views.member_view, name='member'),
 ]
