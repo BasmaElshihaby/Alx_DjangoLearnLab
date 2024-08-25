b375df18bc2964b26481f9d03dbd783ce9342eb4
Abdelrahman-bedier <abdo.bedier@gmail.com>
2024-08-26 00:31:15 +0300

    why not

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Book
+from .models import CustomUser, CustomUserAdmin
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
@@ -10,3 +11,4 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author')
     
 admin.site.register(Book)
+admin.site.register(CustomUser, CustomUserAdmin)
