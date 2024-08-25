8bf5c01c2aa4a4d13d4b81914a97510e93a06749
Abdelrahman-bedier <abdo.bedier@gmail.com>
2024-08-26 00:29:36 +0300

    because we are stupid

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Book(models.Model):
@@ -6,3 +7,9 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+
+class CustomUser(AbstractUser):
+    id = models.AutoField(primary_key=True)
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField()
