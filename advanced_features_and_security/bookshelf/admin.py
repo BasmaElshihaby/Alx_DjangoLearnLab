bb7bca5f31d8d07da46ecc86e49ff9819b27f9ba
Abdelrahman-bedier <abdo.bedier@gmail.com>
2024-08-26 00:26:28 +0300

    we should work on bookshelf

--- advanced_features_and_security/bookshelf/admin.py
+++ advanced_features_and_security/bookshelf/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Book
+from .models import User
+
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
@@ -10,3 +12,5 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author')
     
 admin.site.register(Book)
+
+admin.site.register(User)
