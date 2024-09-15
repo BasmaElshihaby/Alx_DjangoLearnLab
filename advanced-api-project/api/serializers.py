fda29ea0e6b392cb1b166ae2f83cd56332e217f4
Abdelrahman-bedier <abdo.bedier@gmail.com>
2024-09-15 09:50:28 +0300

    validate

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -7,6 +7,10 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = '__all__'  # Serialize all fields of the Book model
 
+    def validate_published_date(self, value):
+        if value > date.today():
+            raise serializers.ValidationError("The publication date cannot be in the future.")
+        return value
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
