8357209c79011e4da1897893735b5b3c98f162e9
Abdelrahman-bedier <abdo.bedier@gmail.com>
2024-09-28 16:47:13 +0300

    implementing urls

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('books/update/', BookUpdateView.as_view(), name='book-update'),
     path('books/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
-#BookList.as_view
+#BookList.as_view DefaultRouter()", "router.urls", "include"
